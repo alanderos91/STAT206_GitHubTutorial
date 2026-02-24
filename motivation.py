bfa69652983967313274a90d7ad7b30f57a7ed05
Alfonso Landeros <alandero@ucr.edu>
2026-02-24 10:13:50 -0800

    Do the correct thing

--- motivation.py
+++ motivation.py
@@ -2,23 +2,21 @@ import pandas as pd
 
 grades = pd.read_json("grades.json")
 new_grades = grades.copy()
+new_grades.set_index("id", inplace=True)
 
 # --------------- BEGIN STUDENT CODE --------------- #
-# Change id jong029 grade to A:
-new_grades.loc[new_grades["id"] == "jong029", "grade"] = "A"
-# Alternate method:
-#new_grades.loc["jong029", "grade"] = "A"
+
+new_grades.loc["jong029", "grade"] = "A"
 new_grades.loc["glin057","grade"] = "A"
-new_grades.loc["Jiaqi","grade"] = "A+"
+new_grades.loc["jwang1155","grade"] = "A+"
 new_grades.loc["jtram003", "grade"] = "A"
-new_grades.loc["flores", "grade"] = "A"
-new_grades.loc[new_grades["student_id"] == "bmeft001", "grade"] = A
-new_grades.loc["mpenu005","grades"]="A"
+new_grades.loc["eflor141", "grade"] = "A"
+new_grades.loc["bmeft001", "grade"] = "A"
+new_grades.loc["mpenu005","grade"]="A"
 new_grades.loc["cluo042","grade"]="A"
-
-
 
 # ---------------- END STUDENT CODE ---------------- #
 
+new_grades.reset_index(inplace=True)
 new_grades.to_json("new_grades.json", index = False)
 new_grades
